2b445069efa072b7699251efa95a5281ee97305c
Francesco Romani <fromani@redhat.com>
2021-01-07 13:04:57 +0100

    pkg: snapshot: move packing code into the pkg
    Signed-off-by: Francesco Romani <fromani@redhat.com>

--- cmd/ghw-snapshot/main.go
+++ cmd/ghw-snapshot/main.go
@@ -8,16 +8,12 @@
 package main
 
 import (
-	"archive/tar"
-	"compress/gzip"
 	"crypto/md5"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"os"
-	"path/filepath"
 	"runtime"
-	"strings"
 
 	"github.com/spf13/cobra"
 
@@ -76,46 +72,15 @@ func execute(cmd *cobra.Command, args []string) error {
 	if err = snapshot.CloneTreeInto(scratchDir); err != nil {
 		return err
 	}
-	return doSnapshot(scratchDir)
-}
 
-func doSnapshot(buildDir string) error {
 	if outPath == "" {
 		outPath = defaultOutPath()
 		trace("using default output filepath %s\n", outPath)
 	}
 
-	var f *os.File
-	var err error
-
-	if _, err = os.Stat(outPath); os.IsNotExist(err) {
-		if f, err = os.Create(outPath); err != nil {
-			return err
-		}
-	} else if err != nil {
-		return err
-	} else {
-		f, err := os.OpenFile(outPath, os.O_WRONLY, 0600)
-		if err != nil {
-			return err
-		}
-		fs, err := f.Stat()
-		if err != nil {
-			return err
-		}
-		if fs.Size() > 0 {
-			return fmt.Errorf("File %s already exists and is of size >0", outPath)
-		}
-	}
-	defer f.Close()
-
-	gzw := gzip.NewWriter(f)
-	defer gzw.Close()
-
-	tw := tar.NewWriter(gzw)
-	defer tw.Close()
-
-	return createSnapshot(tw, buildDir)
+	return snapshot.PackFrom(outPath, scratchDir, snapshot.PackOptions{
+		Compress: true,
+	})
 }
 
 func main() {
@@ -132,44 +97,4 @@ func init() {
 	rootCmd.PersistentFlags().BoolVarP(
 		&debug, "debug", "d", false, "Enable or disable debug mode",
 	)
-}
-
-func createSnapshot(tw *tar.Writer, buildDir string) error {
-	return filepath.Walk(buildDir, func(path string, fi os.FileInfo, err error) error {
-		if path == buildDir {
-			return nil
-		}
-		var link string
-
-		if fi.Mode()&os.ModeSymlink != 0 {
-			trace("processing symlink %s\n", path)
-			link, err = os.Readlink(path)
-			if err != nil {
-				return err
-			}
-		}
-
-		hdr, err := tar.FileInfoHeader(fi, link)
-		if err != nil {
-			return err
-		}
-		hdr.Name = strings.TrimPrefix(strings.TrimPrefix(path, buildDir), string(os.PathSeparator))
-
-		if err = tw.WriteHeader(hdr); err != nil {
-			return err
-		}
-
-		switch hdr.Typeflag {
-		case tar.TypeReg, tar.TypeRegA:
-			f, err := os.Open(path)
-			if err != nil {
-				return err
-			}
-			if _, err = io.Copy(tw, f); err != nil {
-				return err
-			}
-			f.Close()
-		}
-		return nil
-	})
 }
